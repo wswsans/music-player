852c6ab6b6ef21d6177cbf7cc8f59f47d92ad89e
Wsan(笑さん) <32955729+wswsans@users.noreply.github.com>
2021-08-18 22:43:30 +0900

    PWA? 6
    現時点でPWAを突っ込んでるファイルを全部上書きしてみる

--- scriptV1.js
+++ scriptV1.js
@@ -410,6 +410,12 @@ $(() => {
 });
 
 // PWA
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/music-player/sw.js');
-};
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/music-player/sw.js').then(function(registration) {
+		// 登録成功
+		console.log('ServiceWorker registration successful with scope: ', registration.scope);
+	}).catch(function(err) {
+		// 登録失敗 :(
+		console.log('ServiceWorker registration failed: ', err);
+	});
+}
