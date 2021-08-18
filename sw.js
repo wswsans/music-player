7e8523fd28fa92e2533c7965e63b60434ad92c00
Wsan(笑さん) <32955729+wswsans@users.noreply.github.com>
2021-08-18 23:24:56 +0900

    PWA 10
    selected context only
    お前は絶対に許さない

--- sw.js
+++ sw.js
@@ -22,31 +22,43 @@ self.addEventListener('install', function(event) {
   );
 });
 
-// 重要：リクエストを clone する。リクエストは Stream なので
-// 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-// 必要なので、リクエストは clone しないといけない
-var fetchRequest = event.request.clone();
-
-return fetch(fetchRequest).then(
-  function(response) {
-    // レスポンスが正しいかをチェック
-    if(!response || response.status !== 200 || response.type !== 'basic') {
-      return response;
-    }
-
-    // 重要：レスポンスを clone する。レスポンスは Stream で
-    // ブラウザ用とキャッシュ用の2回必要。なので clone して
-    // 2つの Stream があるようにする
-    var responseToCache = response.clone();
-
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        cache.put(event.request, responseToCache);
-      });
-
-    return response;
-  }
-);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // キャッシュがあったのでそのレスポンスを返す
+        if (response) {
+          return response;
+        }
+        // 重要：リクエストを clone する。リクエストは Stream なので
+        // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+        // 必要なので、リクエストは clone しないといけない
+        var fetchRequest = event.request.clone();
+        
+        return fetch(fetchRequest).then(
+          function(response) {
+            // レスポンスが正しいかをチェック
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+        
+            // 重要：レスポンスを clone する。レスポンスは Stream で
+            // ブラウザ用とキャッシュ用の2回必要。なので clone して
+            // 2つの Stream があるようにする
+            var responseToCache = response.clone();
+        
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+        
+            return response;
+          }
+        );
+      }
+    )
+  );
+});
 
 self.addEventListener('activate', function(event) {
   clients.claim();
